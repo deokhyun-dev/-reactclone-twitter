5aed78c36b6bde85def0b85faae387b8868d1ddc
dustin <dev.deokhyun@gmail.com>
2021-10-31 14:06:22 +0900

    Auth CSS 완성

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -3,6 +3,13 @@
 import React, { useState } from "react";
 import { authService, firebaseInstance } from "../fbase";
 import AuthForm from "../components/AuthForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faTwitter,
+  faGoogle,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Auth = (props) => {
   const onSocialClick = async (event) => {
@@ -20,13 +27,19 @@ const Auth = (props) => {
   };
 
   return (
-    <div>
+    <div className="authContainer">
+      <FontAwesomeIcon
+        icon={faTwitter}
+        color={"#04AAFF"}
+        size="3x"
+        style={{ marginBottom: 30 }}
+      />
       <AuthForm />
-      <div>
-        <button onClick={onSocialClick} name="google">
+      <div className="authBtns">
+        <button onClick={onSocialClick} name="google" className="authBtn">
           Continue with Google
         </button>
-        <button onClick={onSocialClick} name="github">
+        <button onClick={onSocialClick} name="github" className="authBtn">
           Continue with Github
         </button>
       </div>
